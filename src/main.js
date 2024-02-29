04a05fa6abf0ae68ff4cf0f9fbe1f99dcfcee0a1
AlexBoyko5 <144361285+AlexBoyko5@users.noreply.github.com>
2024-02-29 13:48:25 +0100

    commit#4
    Add axios

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import iziToast from 'izitoast';
 import SimpleLightbox from 'simplelightbox';
 import axios from 'axios';
 
+
 //!======== Вариант №1 =============================================================
 // document.addEventListener("DOMContentLoaded", function () {
 //     const categoryList = document.getElementById("category-list"); //сылка на список категорий
@@ -16,15 +17,33 @@ import axios from 'axios';
 
 //!======== Вариант №2 ====добавл fetch()  ==============================================
 
+// document.addEventListener("DOMContentLoaded", function () {
+//     const categoryList = document.getElementById("category-list"); //сылка на список категорий
+//     fetch('https://books-backend.p.goit.global/books/category-list')
+//         .then(response => response.json()) // Преобразование ответа в JSON
+//         .then(categories => { // Проходим по категориям
+//             categories.forEach(function (category) {
+//                 const li = document.createElement('li'); // создаем 'li'
+//                 li.textContent = category.list_name; // задаем текст категории
+//                 categoryList.appendChild(li); // добавляем елемент в список
+//             });
+//         })
+// });
+
+//!======== Вариант №3 ==== меняем fetch() на Axios ==============================================
+
 document.addEventListener("DOMContentLoaded", function () {
     const categoryList = document.getElementById("category-list"); //сылка на список категорий
-    fetch('https://books-backend.p.goit.global/books/category-list')
-        .then(response => response.json()) // Преобразование ответа в JSON
-        .then(categories => { // Проходим по категориям
+    axios.get('https://books-backend.p.goit.global/books/category-list')
+        .then(response => {
+            const categories = response.data //днные из ответа 
             categories.forEach(function (category) {
                 const li = document.createElement('li'); // создаем 'li'
                 li.textContent = category.list_name; // задаем текст категории
                 categoryList.appendChild(li); // добавляем елемент в список
             });
+        })
+        .catch(error => {
+            console.error('Error fetching data;', error);
         })
 });
